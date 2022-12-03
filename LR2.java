74120a1f89d22c0d1b448c3f600ad3fe8aafeda6
lex777377737773 <119737335+lex777377737773@users.noreply.github.com>
2022-12-03 20:32:15 +0700

    Лабораторная работа №2

---
+++ LR2.java
@@ -0,0 +1,14 @@
+/* Online Java Compiler and Editor */
+import java.util.Scanner;
+public class HelloWorld{
+     public static void main(String []args){
+         Scanner input = new Scanner(System.in);
+         int i=888;
+         String s="Vasya";
+        System.out.println("Hello, World!");
+        System.out.println(i);
+        System.out.println(s);
+            s=input.nextLine();
+               System.out.println(s);
+     }
+}
